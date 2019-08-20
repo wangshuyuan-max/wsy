ac754c2f0ec7de64dec94ee589d8837e19259a71
wangshuyuan-max <1663500361@qq.com>
2019-08-20 17:00:23 +0800

    测试

---
+++ elasticSearch/src/test/java/com/itheima/Test.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2019/8/18 14:36
+ * @Created by wangshuyuan
+ */
+public class Test {
+    int a;
+    Integer b;
+    public static void main(String[] args) {
+       new Test();
+
+    }
+    public   Test(){
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
